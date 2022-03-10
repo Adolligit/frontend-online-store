2fc2ae735306b52fd9a21409541687a34fdf10ee
Veronica-Alfr <veagalves@gmail.com>
2022-03-10 17:41:33 -0300

    Requisito 5 em progresso

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,60 @@
 import React, { Component } from 'react';
+// import {  } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      contentProduct: [],
+      loading: false,
+    };
+  }
+
+  inputChange = ({ target }) => {
+    const { name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  buttonSearch = async () => {
+    this.setState({
+      loading: true,
+    }, async () => {
+      const categories = await ;
+      this.setState({
+        // conteudo da pesquisa
+        loading: false,
+      });
+    });
+  }
+
   render() {
     return (
-      <header>
+      <div>
         <div>Home</div>
         <input
+          data-testid="query-input"
           type="text"
           name="search"
+          value={ search }
+          onChange={ this.inputChange }
         />
+        <button
+          data-testid="query-button"
+          type="button"
+          name="btn"
+          onClick={ this.buttonSearch }
+        >
+          Pesquisar
+        </button>
         <span
           data-testid="home-initial-message"
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </span>
-      </header>
+      </div>
     );
   }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,13 @@
 export async function getCategories() {
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const jsonResponse = await response.json();
+  console.log(jsonResponse);
   return jsonResponse;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const jsonResponse = await response.json();
+  console.log(jsonResponse);
   return jsonResponse;
 }

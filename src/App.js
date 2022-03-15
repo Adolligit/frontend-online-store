3587a45b5f9d3acba21c796e8b5d6c4934f56649
Rodrigo de Castro <rodrigo.cs.dev@gmail.com>
2022-03-15 16:46:44 -0300

    realizado parcialmente requisito 12

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails';
 import ProductCart from './pages/ProductCart';
 import './App.css';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         {/* <Route exact path="/cart" component={ Cart } /> */}
         <Route exact path="/product-details/:id" component={ ProductDetails } />
         <Route exact path="/product-cart" component={ ProductCart } />
+        <Route exact path="/checkout" component={ Checkout } />
       </Switch>
     </BrowserRouter>
   );
